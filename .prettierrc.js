18b3535a9d969d704e628a26f067fbbab9982f4b
lumpenop <swk5373@gmail.com>
2023-06-08 03:37:23 +0900

    foundation

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  singleQuote: true,
+  jsxBracketSameLine: false,
+  trailingComma: 'all',
+};
---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+interface Props {}
+
+const App: React.FC<Props> = () => {
+  return (
+    <View>
+      <Text>App hi</Text>
+    </View>
+  );
+};
+
+export default App;
